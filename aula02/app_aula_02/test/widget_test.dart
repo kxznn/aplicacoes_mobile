fa932eab9b4876d0515b354d88395e67f4d53cdb
kxznn <kauaroaguiar16@gmail.com>
2026-08-19 22:49:37 -0300

    app

--- aula02/app_aula_02/test/widget_test.dart
+++ aula02/app_aula_02/test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:app_aula_02/main.dart';
+import 'package:appaula02/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
